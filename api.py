43bb89bb2480e8e2971c6e89b05aa10e6d0bbaac
Deviprasad <nayakdeviprasad07@gmail.com>
2019-08-17 16:57:15 +0530

    added products

--- api.py
+++ api.py
@@ -1,5 +1,5 @@
 from flask import Flask,render_template,request,session,redirect,url_for
-from model import check_user,add_user_to_db, add_product_to_db
+from model import check_user,add_user_to_db, add_product_to_db,check_product,get_products,remove_product
 
 app= Flask(__name__)
 app.secret_key='hello'
@@ -17,26 +17,30 @@ def about():
 def contact():
 	return render_template('contact.html',title='contact')
 
-@app.route('/addproduct')
-def addpro():
-	return render_template('addproduct.html',title='addproduc')
+# @app.route('/product')
+# def addpro():
+# 	return render_template('addproduct.html',title='addproduc')
 
 
-@app.route('/add',methods=['GET','POST'])
-def add():
-	if request.method=='POST':
+# @app.route('/products',methods=['GET','POST'])
+# def products():
+# 	if request.method=='POST':
 
-		product_info = {}
+# 		product_info = {}
 
-		product_info['pname'] = request.form['pname']
-		product_info['pprice'] = request.form['pprice']
-		product_info['pdescription'] = request.form['pdescription']
-		product_info['sellername'] = request.form['sellername']
+# 		product_info['pname'] = request.form['pname']
+# 		product_info['pprice'] = int(request.form['pprice'])
+# 		product_info['pdescription'] = request.form['pdescription']
+# 		# product_info['sellername'] = request.form['sellername']
+# 		product_info['seller'] = session['username']
 
-		add_product_to_db(product_info)
-		return redirect(url_for('home'))
+# 		if bool(check_product(product_info['pname'])) is True:
+# 			return "product already exists"
 
-	return redirect(url_for('home'))
+# 		add_product_to_db(product_info)
+# 		return redirect(url_for('home'))
+
+# 	return redirect(url_for('home'))
 
 @app.route('/signup',methods=['GET','POST'])
 def signup():
@@ -62,6 +66,9 @@ def signup():
 			user_info['cart'] = []
 
 		add_user_to_db(user_info)
+		# session['username']=username
+		# session['c_type'] = check_user(username)['c_type']
+
 		return redirect(url_for('home'))
 
 	return redirect(url_for('home'))
@@ -82,6 +89,37 @@ def login():
 			return redirect(url_for('home'))
 		return "Username or password incorrect,Try again"
 	return redirect(url_for('home'))
+
+@app.route('/products',methods=['GET','POST'])
+def products():
+	if request.method=='POST':
+		# import pdb; pdb .set_trace()
+
+		product_info = {}
+
+		product_info['pname'] = request.form['pname']
+		product_info['pprice'] = int(request.form['pprice'])
+		product_info['pdescription'] = request.form['pdescription']
+		# product_info['sellername'] = request.form['sellername']
+		product_info['sellername'] = session['username']
+
+		if bool(check_product(product_info['pname'])) is True:
+			return "product already exists"
+
+		add_product_to_db(product_info)
+		return redirect(url_for('products'))
+
+	products= get_products()
+	return render_template('products.html', products=products)
+
+
+
+@app.route('/remove', methods=['GET','POST'])
+def remove():
+	if request.method=='POST':
+		pname=request.form['pname']
+		remove_product(pname)
+	return redirect(url_for('products'))
 
 @app.route('/logout')
 def logout():
--- model.py
+++ model.py
@@ -1,4 +1,5 @@
 from pymongo import MongoClient
+from flask import session
 
 client= MongoClient()
 db = client['amazon_valley']
@@ -13,6 +14,31 @@ def add_user_to_db(user_info):
 	
 	db['users'].insert_one(user_info) 
 
+
+def check_product(pname):
+
+	query ={'pname':pname}
+	result =db['product'].find_one(query)
+	return result
+
 def add_product_to_db(product_info):
 	
 	db['product'].insert_one(product_info) 
+
+def get_products():
+
+	if session['c_type'] == 'buyer':
+		result= db['product'].find({})
+		return result
+
+
+	query={"sellername":session['username']}
+	result=db['product'].find(query)
+	return result
+
+
+def remove_product(pname):
+
+	query = {"pname":pname}
+	db['product'].remove(query)
+
